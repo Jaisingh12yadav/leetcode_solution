c168cc4444dfce2f0f7ebab11360c6510ae9f1b8
Jaisingh12yadav <164960173+Jaisingh12yadav@users.noreply.github.com>
2026-09-04 07:44:38 +0530

    Time: 1 ms (98.1%), Space: 42.5 MB (80.36%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,24 +1,13 @@
-// class Solution {
-//     public int addDigits(int num) {
-//         int sum =0;
-//         int ans = 0;
-//         while(num>0){
-//             int digit = num%10;
-//             sum+=digit;           
-//            num = num/10;        
-//     }
-//         while(sum>0){
-//             int digit = sum%10;
-//             ans = ans + digit;
-//             sum = sum/10;
-//         }
-//         return ans;
-//     }
-// }
-
 class Solution {
     public int addDigits(int num) {
-        if (num == 0) return 0;
-        return 1 + (num - 1) % 9;
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
     }
 }
