151a4501df5f5d0887f051a56107171409ada903
Jaisingh12yadav <164960173+Jaisingh12yadav@users.noreply.github.com>
2026-09-05 12:45:35 +0530

    Time: 0 ms (100%), Space: 43.2 MB (23.71%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -1,20 +1,37 @@
 
+// class Solution {
+//     public int sumOddLengthSubarrays(int[] arr) {
+//         int sum = 0;
+//         int n = arr.length;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i; j < n; j++) {
+//                 int length = j - i + 1;
+//                 if (length % 2 == 1) {
+//                     for (int k = i; k <= j; k++) {
+//                         sum += arr[k];
+//                     }
+//                 }
+//             }
+//         }
+
+//         return sum;
+//     }
+// }
+
+
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
-        int sum = 0;
         int n = arr.length;
+        int sum = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                int length = j - i + 1;
-                if (length % 2 == 1) {
-                    for (int k = i; k <= j; k++) {
-                        sum += arr[k];
-                    }
-                }
-            }
+            int left = i + 1;
+            int right = n - i;
+            int total = left * right;
+            int odd = (total + 1) / 2;
+
+            sum += arr[i] * odd;
         }
 
         return sum;
     }
 }
-
