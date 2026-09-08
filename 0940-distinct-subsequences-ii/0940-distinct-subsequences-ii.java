1c473f4caf16ea9fdc7a772454ab67018d38c89f
Jaisingh12yadav <164960173+Jaisingh12yadav@users.noreply.github.com>
2026-09-08 09:46:51 +0530

    Time: 5 ms (52.92%), Space: 43.2 MB (70.79%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int distinctSubseqII(String s) {
+
+        int MOD = 1_000_000_007;
+
+        int[] dp = new int[26];
+
+        for (char ch : s.toCharArray()) {
+
+            int index = ch - 'a';
+
+            int sum = 0;
+
+            for (int x : dp) {
+                sum = (sum + x) % MOD;
+            }
+
+            dp[index] = (sum + 1) % MOD;
+        }
+
+        int ans = 0;
+
+        for (int x : dp) {
+            ans = (ans + x) % MOD;
+        }
+
+        return ans;
+    }
+}
